de1a3ebe7d92f7be61e7b2a5033e2313b7f6b581
grego1201 <gregoriopatinoesteo@gmail.com>
2019-02-01 23:12:55 +0100

    Add flameshot to install

---
+++ install.sh
@@ -0,0 +1,5 @@
+# install flameshot
+
+wget "https://launchpad.net/~harish2704/+archive/ubuntu/ppa1/+files/flameshot_0.5.0_amd64.deb"
+sudo dpkg -i flameshot_0.5.0_amd64.deb
+rm flameshot_0.5.0_amd64.deb
