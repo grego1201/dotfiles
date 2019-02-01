1559584a9b109eeaed0235cd3e24df87e4470914
grego1201 <gregoriopatinoesteo@gmail.com>
2019-02-01 23:10:53 +0100

    update connection method and change sudo password input

--- conectar_altavoz.sh
+++ conectar_altavoz.sh
@@ -1,8 +1,7 @@
 # HOW TO USE
 #
-# this script recive two arguments, first is sudo password (be carefull and delete last command in log) and the second one device ID
-# an example of use is:
-# sh conectar_altavoz.sh "MyPassword" "11:22:33:AA:BB:CC"
+# this script recive onw argument, an example of use is this:
+# sh conectar_altavoz.sh "11:22:33:AA:BB:CC"
 #
 #
 # PREVIOUS CONFIGS
@@ -18,39 +17,38 @@ VAR1="$(tmux list-sessions | grep "ConnectBluetooth" | wc -l)"
 if [ $VAR1 = 0 ]
 then
   echo "Creo el panel"
-  tmux new-session -d -s ConnectBluetooth 'sudo bluetoothctl -a '
+  tmux new-session -d -s ConnectBluetooth
 fi
 sleep 1
 
-echo "Busco el panel"
-tmux send-keys -t ConnectBluetooth $1 Enter
+tmux send-keys -t ConnectBluetooth "sudo service bluetooth restart" Enter
 sleep 1
-echo "Conecto el dispositivo"
-tmux send-keys -t ConnectBluetooth "connect $2" Enter
-
-HIGH_QUALITY="$(pacmd list-sinks | awk '/name:/ {print $2};' | grep "a2dp" | wc -l)"
-echo "$(pacmd list-sinks | awk '/name:/ {print $2};')"
-echo $HIGH_QUALITY
-while [ $HIGH_QUALITY = 0 ]
-do
-  echo "Desconecto el dispositivo"
-  tmux send-keys -t ConnectBluetooth "disconnect $2" Enter
-  sleep 7
-  echo "Reinicio servicio bluetooth"
-  tmux send-keys -t ConnectBluetooth "sudo service bluetooth restart" Enter
-  sleep 3
-  echo "Abro el server"
-  tmux send-keys -t ConnectBluetooth "sudo bluetoothctl -a " Enter
-  sleep 3
-  tmux send-keys -t ConnectBluetooth $1 Enter
-  echo "Conectando en bucle"
-  tmux send-keys -t ConnectBluetooth "connect $2" Enter
-  sleep 5
-  CARD_INDEX="$(pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}')"
-  sleep 3
-  tmux send-keys -t ConnectBluetooth "pacmd set-card-profile $CARD_INDEX a2dp_sink"
-  sleep 3
-  HIGH_QUALITY="$(pacmd list-sinks | awk '/name:/ {print $2};' | grep "a2dp" | wc -l)"
-done
+stty -echo
+read -p "Sudo Password: " passw; echo
+stty echo
+tmux send-keys -t ConnectBluetooth $passw Enter
+sleep 1
+tmux send-keys -t ConnectBluetooth "sudo bluetoothctl -a" Enter
+sleep 1
+tmux send-keys -t ConnectBluetooth "connect $1" Enter
+sleep 5
+tmux send-keys -t ConnectBluetooth "exit" Enter
+sleep 1
+CARD_INDEX="$(pacmd list-cards | grep bluez_card -B1 | grep index | awk '{print $2}')"
+sleep 2
+tmux send-keys -t ConnectBluetooth "pacmd set-card-profile $CARD_INDEX a2dp_sink" Enter
+sleep 1
+SINK_INDEX="$(pacmd list-sinks | grep "bluez_sink" -B1 | awk '/index:/ {print $2}')"
+tmux send-keys -t ConnectBluetooth "pacmd set-default-sink $SINK_INDEX" Enter
 
 echo "Conectado con alta calidad"
+
+
+# TODO
+# This script is still WIP
+# Last thing to develop is to change sink-inputs, apps that are playing some sound, move to
+# the sink. An example of move is this:
+# pacmd move-sink-input 1 14
+# echo $SINK_INDEX
+# 1 --> sink input
+# 14 --> sinks index
