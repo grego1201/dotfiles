67a14d5eef99a501530ad374cd920af83d3e7a37
grego1201 <gregoriopatinoesteo@gmail.com>
2022-01-02 23:08:37 +0100

    Remove old dotfiles

--- move_to_bluetooth.sh
+++
@@ -1,5 +0,0 @@
-SINK_INDEX="$(pacmd list-sinks | grep "bluez_sink" -B1 | awk '/index:/ {print $3}')"
-ALSA_INDEX=$(pacmd list-sink-inputs | grep "alsa_output" -B4 | awk '/index:/ {print $2}')
-
-
-pacmd move-sink-input $ALSA_INDEX $SINK_INDEX
